3daeec81b380856b91a888eb98db9da59d39894d
Sergey Zaharchenko <zaharchenko.dev@gmail.com>
2018-02-13 23:14:22 +0200

    Front controller

--- index.php
+++ index.php
@@ -2,7 +2,19 @@
 
 require_once 'autoload.php';
 
-$newsController = new \App\Controllers\NewsController();
+$request = $_SERVER['REQUEST_URI'];
 
-$newsController->action('Index');
+$request = trim($request, '/');
 
+if ($request) {
+    $request        = explode('/', $request);
+    $controllerName = '\\App\\Controllers\\'.ucfirst($request[0]).'Controller';
+    $action         = ucfirst($request[1]);
+}
+
+$action         = $action ?? 'Index';
+$controllerName = $controllerName ?? '\\App\\Controllers\\NewsController';
+
+$controller = new $controllerName();
+/**@var \App\Controller $controller */
+$controller->action($action);
